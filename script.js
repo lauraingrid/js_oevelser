012dc72fdae80407853b16a8251d31cad0cc9775
lauraingrid <laura.weile@outlook.dk>
2024-01-31 10:59:24 +0100

    øvelse 2 done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* øvelse 1 */
+
 document.addEventListener("DOMContentLoaded", function () {
   const produkter = document.querySelectorAll(".produkt");
 
@@ -17,3 +19,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+/* øvelse 2 */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const klokken = document.querySelector(".klokken");
+
+  klokken.addEventListener("click", function () {
+    var randomTime = Math.random() * 24;
+
+    function hilsen(time) {
+      if (time >= 5 && time < 10) {
+        console.log("Godmorgen");
+      } else if (time >= 10 && time < 18) {
+        console.log("Goddag");
+      } else if (time >= 18 && time < 24) {
+        console.log("Godaften");
+      } else {
+        console.log("Godnat");
+      }
+    }
+
+    hilsen(randomTime);
+    console.log("Klokken er " + randomTime.toFixed(2) + ": ");
+  });
+});
+
+/* øvelse 3 */
